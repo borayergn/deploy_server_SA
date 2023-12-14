f26487d9689b78ec81bcc6cd59d036351e8f99c3
Boray <borayergn10@outlook.com>
2023-12-14 16:53:13 +0300

    static config

--- backend/settings.py
+++ backend/settings.py
@@ -209,11 +209,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 # Default primary key field type
