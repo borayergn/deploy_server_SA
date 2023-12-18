a9a505e3ca5b42ab58c020fc24cd677b90d8eaa6
Boray <borayergn10@outlook.com>
2023-12-18 10:34:24 +0300

    domain changes

--- backend/settings.py
+++ backend/settings.py
@@ -27,9 +27,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 SECRET_KEY = 'django-insecure--d!fqz$+o*dny8lh4g&ooy5&0g^bj8awblgqq716hcau41c_yh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1","django-server-env.eba-yye9dqwq.eu-north-1.elasticbeanstalk.com"]
+ALLOWED_HOSTS = []
 
 SESSION_ENGINE = "django.contrib.sessions.backends.db" 
 SESSION_SAVE_EVERY_REQUEST = True
@@ -203,16 +203,12 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
-
-# Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
